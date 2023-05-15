0be1a0b9e6d2271edf701d358c96a1e6bb577d92
YigitTuncer07 <yigit_tuncer03@hotmail.com>
2023-05-15 15:17:15 +0300

    Added depth and a few more functions

--- src/Parser.java
+++ src/Parser.java
@@ -12,32 +12,35 @@ public class Parser{
 
     public void parse(){
         index = 0;
-        program();
+        program(0);
         
     }
 
-    private void program(){
-        print(0, "<Program>");
+    // Depth represents how deep the function is in the call stack. It doesnt actually do anything,
+    //it only helps while printing.
+    
+    private void program(int depth){
+        print(depth, "<Program>");
 
         if (isCurrentToken("LEFTPAR")){
-            topLevelForm();
-            program();
+            topLevelForm(depth + 1);
+            program(depth + 1);
         } else if (index == tokens.length - 1){//This means code has ended
-            print(1,"___");
+            print(depth + 1,"___");
         } else {
             announceError("(");
         }
     }
 
-    private void topLevelForm(){
-        print(1,"<TopLevelForm>");
+    private void topLevelForm(int depth){
+        print(depth,"<TopLevelForm>");
 
         if (isCurrentToken("LEFTPAR")){
-            print(2,currentTokenType,"(");
+            print(depth + 1,currentTokenType,"(");
             getNextToken();
-            secondLevelForm();
+            secondLevelForm(depth + 1);
             if (isCurrentToken("RIGHTPAR")){
-                print(2, currentTokenType, ")");
+                print(depth + 1, currentTokenType, ")");
                 getNextToken();
             } else {
                 announceError(")");
@@ -45,17 +48,17 @@ public class Parser{
         }
     }
 
-    private void secondLevelForm(){
-        print(2, "<SecondLevelForm>");
+    private void secondLevelForm(int depth){
+        print(depth, "<SecondLevelForm>");
 
         if (isCurrentToken("DEFINE")){
-            definition();
+            definition(depth + 1);
         } else if (isCurrentToken("LEFTPAR")){
-            print(3, currentTokenType, "(");
+            print(depth + 1, currentTokenType, "(");
             getNextToken();
-            funCall();
+            funCall(depth + 1);
             if (isCurrentToken("RIGHTPAR")){
-                print(3, currentTokenType,")");
+                print(depth + 1, currentTokenType,")");
                 getNextToken();
             } else {
                 announceError(")");
@@ -65,39 +68,39 @@ public class Parser{
         }
     }
 
-    private void definition(){
-        print(3, "<Definition>");
+    private void definition(int depth){
+        print(depth, "<Definition>");
 
         if (isCurrentToken("DEFINE")){
-            print(4, currentTokenType, "define");
+            print(depth + 1, currentTokenType, "define");
             getNextToken();
-            definitionRight();
+            definitionRight(depth + 1);
         } else {
             announceError("define");
         }
     }
 
-    private void definitionRight(){
-        print(4, "<DefinitionRight>");
+    private void definitionRight(int depth){
+        print(depth, "<DefinitionRight>");
 
         if (isCurrentToken("IDENTIFIER")){
-            print(5, currentTokenType, currentToken.getValue());
+            print(depth + 1, currentTokenType, currentToken.getValue());
             getNextToken();
-            expressions();
+            expressions(depth + 1);
         } else if (isCurrentToken("LEFTPAR")){
-            print(5, currentTokenType, "(");
+            print(depth + 1, currentTokenType, "(");
             getNextToken();
             if (isCurrentToken("IDENTIFIER")){
-                print(5, currentTokenType, currentToken.getValue());
+                print(depth + 1, currentTokenType, currentToken.getValue());
                 getNextToken();
             } else {
                 announceError("identifier");
             }
-            argList();
+            argList(depth + 1);
             if (isCurrentToken("RIGHTPAR")){
-                print(5, currentTokenType, ")");
+                print(depth + 1, currentTokenType, ")");
                 getNextToken();
-                statements();
+                statements(depth + 1);
 
             } else {
                 announceError("RIGHTPAR");
@@ -108,29 +111,30 @@ public class Parser{
         }
     }
 
-    private void expressions(){
-        print(5, "<Expressions>");
+    private void expressions(int depth){
+        print(depth, "<Expressions>");
 
         if (isCurrentToken("IDENTIFIER") || isCurrentToken("NUMBER") || isCurrentToken("CHAR") || isCurrentToken("BOOLEAN") || isCurrentToken("STRING") || isCurrentToken("LEFTPAR")) {
-            expression();
-            expressions();
+            expression(depth + 1);
+            expressions(depth + 1);
         } else {
-
+            print(depth + 1, "___");
         }
 
     }
 
-    private void expression(){
-        print(6, "<Expression>");
+    private void expression(int depth){
+        print(depth, "<Expression>");
 
         if (isCurrentToken("IDENTIFIER") || isCurrentToken("NUMBER") || isCurrentToken("CHAR") || isCurrentToken("BOOLEAN") || isCurrentToken("STRING")) {
-
-        } else if (isCurrentToken("LEFTPAR")) {
-            print(7, currentTokenType, "(");
+            print(depth + 1, currentTokenType, currentToken.getValue());
             getNextToken();
-            expr();
+        } else if (isCurrentToken("LEFTPAR")) {
+            print(depth + 1, currentTokenType, "(");
+            getNextToken();
+            expr(depth + 1);
             if (isCurrentToken("RIGHTPAR")){
-                print(7, currentTokenType, ")");
+                print(depth + 1, currentTokenType, ")");
                 getNextToken();
             } else {
                 announceError(")");
@@ -141,25 +145,131 @@ public class Parser{
         }
     }
 
-    private void expr(){
-        // print(7, "<Expr>");
+    private void expr(int depth){
+        print(depth, "<Expr>");
+
+        if (isCurrentToken("LET")){
+            letExpression(depth + 1);
+        } else if (isCurrentToken("COND")){
+            condExpression(depth + 1);
+        } else if (isCurrentToken("IF")){
+            ifExpression(depth + 1);
+        } else if (isCurrentToken("BEGIN")){
+            beginExpression(depth + 1);
+        } else if (isCurrentToken("IDENTIFIER")){
+            funCall(depth + 1);
+        } else {
+            announceError("let or cond or if or begin or identifier");
+        }
 
     }
 
-    private void argList(){
-        // print(5, "<ArgList>");
+    private void condExpression(int depth){
+        print(depth,"<condExpression>");
+
+        if (isCurrentToken("COND")){
+            print(depth + 1, currentTokenType, "cond");
+            getNextToken();
+
+            condBranches(depth + 1);
+        } else {
+            announceError("cond");
+        }
+    }
+
+    private void condBranches(int depth){
+        print(depth, "<condBranches>");
+
+        if (isCurrentToken("(")){
+            print(depth + 1, currentTokenType, "(");
+            getNextToken();
+
+            expression(depth + 1);
+            statements(depth + 1);
+
+            if (isCurrentToken(")")){
+                print(depth + 1, currentTokenType, ")");
+                getNextToken();
+
+                condBranches(depth);
+            } else {
+                announceError(")");
+            }
+        } else {
+            announceError("(");
+        }
+    }
+
+    private void letExpression(int depth){
+        print(depth,"<LetExpression>");
+
+        if (isCurrentToken("LET")){
+            print(depth + 1, currentTokenType, "let");
+            getNextToken();
+            letExpr(depth + 1);
+        } else {
+            announceError("let");
+        }
+    }
+
+    private void letExpr(int depth){
+        print(depth, "<LetExpr>");
+    }
+
+    private void varDefs(int depth){
 
     }
 
-    private void statements(){
-        // print(5, "<Statements>");
+    private void ifExpression(int depth){
+        print(depth,"<IfExpression>");
+    }
+
+    private void beginExpression(int depth){
+        print(depth,"<BeginExpression>");
+    }
+
+    private void funCall(int depth){
+        print(depth,"<FunCall>");
+
+        if (isCurrentToken("IDENTIFIER")){
+            print(depth + 1, currentTokenType, currentToken.getValue());
+            getNextToken();
+            expressions(depth);
+        } else {
+            announceError("identifier");
+        }
 
     }
 
-    private void funCall(){
-        // print(3, "<FunCall>");
+    private void argList(int depth){
+        print(depth, "<ArgList>");
+
+        if (isCurrentToken("IDENTIFIER")){
+            print(depth + 1, currentTokenType, currentToken.getValue());
+            getNextToken();
+        } else {
+            print(depth + 1, "___");
+        }
 
     }
+
+    private void statements(int depth){
+        print(depth, "<Statements>");
+
+        if (isCurrentToken("IDENTIFIER") || isCurrentToken("NUMBER") || isCurrentToken("CHAR") || isCurrentToken("BOOLEAN") || isCurrentToken("STRING") || isCurrentToken("LEFTPAR")){
+            expression(depth + 1);
+        } else if (isCurrentToken("DEFINE")){
+            definition(depth + 1);
+            statements(depth + 1);
+        } else {
+            announceError("identifier or number or char or boolean or string or leftpar or define");
+        }
+
+    }
+
+
+    //----------------------------------------------------------------------
+
     private void print(int tabs, String string, String value){
         for (int i = 0; i < tabs; i++){
             System.out.print("\t");
